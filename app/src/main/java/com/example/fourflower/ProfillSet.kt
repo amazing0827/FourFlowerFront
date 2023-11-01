1bbdbb2f3c9443fed62f4c9dfe673e2807b6be3b
Jiye-Park01 <jiye0710@naver.com>
2023-11-01 13:09:02 +0900

    박지예 11월 1일 (마이페이지)

---
+++ app/src/main/java/com/example/fourflower/ProfillSet.kt
@@ -0,0 +1,11 @@
+package com.example.fourflower
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfillSet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profill_set)
+    }
+}
