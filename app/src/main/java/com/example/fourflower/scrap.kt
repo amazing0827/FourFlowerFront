2b382532f952776c4d35d41e176ec1aa7dd4b595
Jiye-Park01 <jiye0710@naver.com>
2023-11-03 05:16:44 +0900

    11/3 추가

---
+++ app/src/main/java/com/example/fourflower/scrap.kt
@@ -0,0 +1,11 @@
+package com.example.fourflower
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class scrap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrap)
+    }
+}
